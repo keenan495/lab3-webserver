8b7e54c81342c4fa0a7cbf6ca1eed649faa61176
keenan495 <55205303+keenan495@users.noreply.github.com>
2019-12-16 12:38:55 -0500

    try 14

--- newpage.php
+++ newpage.php
@@ -13,6 +13,7 @@
             $database= "final";
      ?>
     <form action="https://www.google.com/search?q=" method="get">
+    header('Location: https//www.google.com/search?q='.$search);
    <!-- <form method="get" action="https://www.google.com/search?q="> -->
     <center>search: <input type="text" name="search"></center>  <br/> 
     <center> <input type="submit" value="search" onclick="send2()">  <input type="submit" value="I'm feeling lucky"></center> 
