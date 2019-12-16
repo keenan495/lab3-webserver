5324dac6a62c9baf31e7f368741ddf1cef8ae3b5
keenan495 <55205303+keenan495@users.noreply.github.com>
2019-12-16 11:09:52 -0500

    try3

--- newpage.php
+++ newpage.php
@@ -4,9 +4,8 @@
     <title> WEB SERVER FINAL EXAM   </title> 
   
     <body>
-        <h3> Enter Workout type: 
-            <img src= "https://images-na.ssl-images-amazon.com/images/I/71Z7ATk4CIL._SL1500_.jpg" width=500 height=500><br/>
-
+        <h3> WEB SERVER FINAL EXAM
+            <img src= "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstorage.googleapis.com%2Fgd-wagtail-prod-assets%2Fimages%2Fevolving_google_identity_2x.max-4000x2000.jpegquality-90.jpg&imgrefurl=https%3A%2F%2Fdesign.google%2Flibrary%2Fevolving-google-identity%2F&docid=4aQ0r7NfC0dsEM&tbnid=qZcGMELPKtbGvM%3A&vet=10ahUKEwjciuulxrrmAhUDZN8KHZlBCXsQMwh5KAAwAA..i&w=2432&h=1216&bih=901&biw=1564&q=google%20image&ved=0ahUKEwjciuulxrrmAhUDZN8KHZlBCXsQMwh5KAAwAA&iact=mrc&uact=8" width=500 height=500><br/>
 
             <?php $server = "localhost";
             $username = "php";
@@ -15,7 +14,9 @@
      ?>
 
     search: <input type="text" name="search"> <br/> 
-            your info is:
+    <input type="submit" value="enter">
+         
+        your info is:
             
             <?php
                 $search= htmlspecialchars($_GET['search']);
