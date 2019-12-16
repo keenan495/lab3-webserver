f793750279c6faca7cf0c1dd447d2f3493834489
keenan495 <55205303+keenan495@users.noreply.github.com>
2019-12-16 11:32:06 -0500

    try again

--- newindex.php
+++ newindex.php
@@ -24,7 +24,7 @@
    
                 <br/>
                 <br/>
-              What is a dinosaur? :  
+              What is a dinosaur? : 
               Dinosaurs are a diverse group of reptiles of the clade Dinosauria. 
               They first appeared during the Triassic period, between 243 and 233.23 million years ago, 
               although the exact origin and timing of the evolution of dinosaurs is the subject of active research. 
--- newpage.php
+++ newpage.php
@@ -5,12 +5,12 @@
   
     <body>
         <h3> WEB SERVER FINAL EXAM
-            <img src= "https://www.google.com/url?sa=i&source=imgres&cd=&ved=2ahUKEwjuhb2xxrrmAhXLQs0KHXYCBa8QjRx6BAgBEAQ&url=https%3A%2F%2Fdesign.google%2Flibrary%2Fevolving-google-identity%2F&psig=AOvVaw119FSyVG1ui0e9ETZ73Y30&ust=1576598809339460" width=500 height=500><br/>
+            <img src= "" width=500 height=500><br/>
 
             <?php $server = "localhost";
-            $username = "php";
-            $password= "password";
-            $database= "test";
+            $username = "root";
+            $password= "staticvoid";
+            $database= "final";
      ?>
     <form action="newpage.php" method="get">
     search: <input type="text" name="search"> <br/> 
@@ -26,6 +26,22 @@
             <?php echo $_SERVER['REMOTE_ADDR']; ?> and
             <?php echo $_GET['search'];
             ?>
+
+
+<?php
+        $conn = mysqli_connect($server, $username, $password, $database);  
+        $ID1=001
+        $sql = "INSERT INTO googlesearches(id, search, ip) VALUES('$ID1','$search', '$ip');";
+
+        if (mysqli_query($conn,$sql)) {
+
+        echo "it worked";
+        }
+        else{
+           echo "error:" .$sql . "<br>" . mysqli_error($conn);
+        }
+        mysqli_close($conn);
+    ?>
 
     </form>
         <br/>
